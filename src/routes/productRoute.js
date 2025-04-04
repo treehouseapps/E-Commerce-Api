36767d14067685e99acb64a37c36d7fcab16f3f1
Beki Junior <bbekijunior1@gmail.com>
2025-04-04 23:14:07 +0300

    updating

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -5,9 +5,9 @@ const { isAdmin, isUser } = require('../middleware/authMddleware')
 const { addProduct, searchProducts, getAllProducts, getProduct, updateProduct, deleteProduct } = require('../controller/productController')
 
 app.post('/products/add', isAdmin, addProduct)
-app.post('/products/search', isUser, searchProducts)
-app.get('/products/', isUser, getAllProducts)
-app.get('/products/:id', isUser, getProduct)
+app.post('/products/search', searchProducts)
+app.get('/products/', getAllProducts)
+app.get('/products/:id', getProduct)
 app.put('/products/:id', isAdmin, updateProduct)
 app.delete('/products/:id', isAdmin, deleteProduct)
 
