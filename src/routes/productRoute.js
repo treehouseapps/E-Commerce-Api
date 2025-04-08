5788d7fbdd0b1be68be10129c973c2676f8072cf
Beki Junior <bbekijunior1@gmail.com>
2025-04-08 16:53:34 +0300

    update

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,7 +4,7 @@ const { addProduct, searchProducts, getAllProducts, getProduct, updateProduct, d
 const { isAdmin } = require('../middleware/authMddleware')
 /**
  * @swagger
- * /api/products:
+ * /products/add:
  *   post:
  *     summary: Add a new product
  *     description: Add a new product to the inventory.
@@ -40,7 +40,7 @@ app.post('/products/add', isAdmin, addProduct)
 
 /**
  * @swagger
- * /api/products:
+ * /products/:
  *   get:
  *     summary: Get all products
  *     description: Retrieve all products from the inventory.
@@ -55,7 +55,7 @@ app.get('/products/', getAllProducts)
 
 /**
  * @swagger
- * /api/products/{id}:
+ * /products/{id}:
  *   get:
  *     summary: Get a single product by ID
  *     description: Retrieve product details by its unique ID.
@@ -79,7 +79,7 @@ app.get('/products/:id', getProduct)
 
 /**
  * @swagger
- * /api/products/search:
+ * /products/search:
  *   post:
  *     summary: Search products by name
  *     description: Perform a case-insensitive search for products by name.
@@ -107,7 +107,7 @@ app.post('/products/search', searchProducts)
 
 /**
  * @swagger
- * /api/products/{id}:
+ * /products/{id}:
  *   put:
  *     summary: Update a product
  *     description: Update product details by its unique ID.
@@ -147,7 +147,7 @@ app.put('/products/:id', isAdmin, updateProduct)
 
 /**
  * @swagger
- * /api/products/{id}:
+ * /products/{id}:
  *   delete:
  *     summary: Delete a product by ID
  *     description: Delete a product from the inventory by its unique ID.
