2bdafcedc2fa738419a60d5861a93de8c25cbcd1
Beki Junior <bbekijunior1@gmail.com>
2025-04-17 16:03:15 +0300

    adding cors'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require("dotenv").config();
+const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/config')
 const authRoutes = require('./routes/userRoute');
@@ -13,6 +14,7 @@ const swaggerUi = require('swagger-ui-express');
 connectDB();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
