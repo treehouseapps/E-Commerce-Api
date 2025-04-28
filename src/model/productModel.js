12197b166f68ccc6e0c9eb0f708632fdee2fd11d
Beki Junior <bbekijunior1@gmail.com>
2025-04-28 11:42:48 +0300

    updating products

--- src/model/productModel.js
+++ src/model/productModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
+    img: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true }
