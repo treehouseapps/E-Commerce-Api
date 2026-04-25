edb6858454b46f0c3663f512d07b691694a0683b
treehouseapps <bbekijunior1@gmail.com>
2026-04-25 14:16:39 +0300

    fixing mongo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./src/app')
+const app = require("./src/app");
 
 app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}...`)
-})
+  console.log(`server running on port ${process.env.PORT}...`);
+});
--- src/middleware/authMddleware.js
+++ src/middleware/authMddleware.js
@@ -2,44 +2,58 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const isUser = (req, res, next) => {
-    const token = req.header("Authorization");
-    // Check if token exists
-    if (!token) {
-        return res.status(401).json({ message: "Access Denied! No token provided." });
-    }
+  const token = req.header("Authorization");
+  // Check if token exists
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied! No token provided." });
+  }
 
-    try {
-        // Verify token
-        const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-        req.user = decoded;
+  try {
+    // Verify token
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET,
+    );
+    req.user = decoded;
 
-        if (req.user.role === 'user') {
-            next();
-        } else {
-            return res.status(403).json({ message: "Access Denied! User role required." });
-        }
-    } catch (err) {
-        res.status(400).json({ message: "Invalid Token" });
+    if (req.user.role === "user") {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "Access Denied! User role required." });
     }
+  } catch (err) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
 };
 const isAdmin = (req, res, next) => {
-    const token = req.header("Authorization");
-    // Check if token exists
-    if (!token) {
-        return res.status(401).json({ message: "Access Denied! No token provided." });
-    }
+  const token = req.header("Authorization");
+  // Check if token exists
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied! No token provided." });
+  }
 
-    try {
-        const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-        req.user = decoded;
+  try {
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET,
+    );
+    req.user = decoded;
 
-        if (req.user.role === 'admin') {
-            next();
-        } else {
-            return res.status(403).json({ message: "Access Denied! User role required." });
-        }
-    } catch (err) {
-        res.status(400).json({ message: "Invalid Token" });
+    if (req.user.role === "admin") {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "Access Denied! User role required." });
     }
-}
+  } catch (err) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
+};
 module.exports = { isAdmin, isUser };
